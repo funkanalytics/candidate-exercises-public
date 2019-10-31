95ae0d881d50cc3dc20629a24c922e0c72fa81d9
Aaron Funk Taylor <funkanalytics@gmail.com>
2019-10-31 10:42:53 -0700

    initial stream service for population data

---
+++ src/test/kotlin/com/imply/architect/candidateapplication/CandidateApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.imply.architect.candidateapplication
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CandidateApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
