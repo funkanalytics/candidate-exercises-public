3ab1821a5a170459a6fa803a151eec2d30287ce4
Aaron Funk Taylor <funkanalytics@gmail.com>
2019-10-31 08:31:54 -0700

    initial commit

---
+++ src/test/java/com/imply/architect/candidateexercise/CandidateExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imply.architect.candidateexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
